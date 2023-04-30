d53f5024797d5b02653910cb9ddef6aae978fb30
kurta17 <114169244+kurta17@users.noreply.github.com>
2023-04-30 17:55:39 +0400

    Add files via upload

---
+++ UniLab.py
@@ -0,0 +1,27 @@
+#print("Hello, world")
+#print("Giorgi kurtanidze")
+
+#teqsti = "Hello, world! I am George Kurtanidze, 18 years old"
+#print(teqsti.find("Hello"))
+#print(teqsti.endswith("dze"))
+#print(teqsti.isdigit())
+#print("18".isdigit())
+#print(teqsti.split(" "))
+
+#list=['Hello,', 'world!', 'I', 'am', 'George', 'Kurtanidze,', '18', 'years', 'old']
+#rint(" - ".join(list))
+
+#str1="I am "
+#num=18
+#str2=" years old."
+#print(f"{str1}{num}{str2}")
+
+text= "kurta"
+list=['Hello,', 'world!', 'I', 'am', 'George', 'Kurtanidze,', '18', 'years', 'old']
+#print(list.index("I"))
+#print(list[5])
+list.append("18")
+#print(list.count("18"))
+#print(list)
+list.reverse()
+print(list)
